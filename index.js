764c03463169cf0c47155d1ecca999a1d27f922f
TY-8401 <148982902+TY-8401@users.noreply.github.com>
2025-06-18 17:23:48 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const requireAsset = require.context('.', true, /^\.\/(\w+)\/([\w-]+)\.json$/, 'lazy')
+import { clientConfig } from '@/lib/server/config'
 
-/**
- * Lazy-load lang data
- *
- * @param {string} section - The section of lang data to load
- * @param {string} lang    - The language name
- * @returns {Promise<object>} - The content of a lang JSON
- */
-export default function loadLocale (section, lang) {
-  return requireAsset(`./${section}/${lang}.json`)
+import Container from '@/components/Container'
+import BlogPost from '@/components/BlogPost'
+import Pagination from '@/components/Pagination'
+import { getAllPosts } from '@/lib/notion'
+import { useConfig } from '@/lib/config'
+
+export async function getStaticProps () {
+  const posts = await getAllPosts({ includePages: false })
+  const postsToShow = posts.slice(0, clientConfig.postsPerPage)
+  const totalPosts = posts.length
+  const showNext = totalPosts > clientConfig.postsPerPage
+  return {
+    props: {
+      page: 1, // current page is 1
+      postsToShow,
+      showNext
+    },
+    revalidate: 1
+  }
+}
+
+export default function Blog ({ postsToShow, page, showNext }) {
+  const { title, description } = useConfig()
+
+  return (
+    <Container title={title} description={description}>
+      {postsToShow.map(post => (
+        <BlogPost key={post.id} post={post} />
+      ))}
+      {showNext && <Pagination page={page} showNext={showNext} />}
+    </Container>
+  )
 }
